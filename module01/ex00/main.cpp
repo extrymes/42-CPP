82c9f0d1af5fd3c697b9102c4e9b4cc91a98bbcb
extrymes <65893656+extrymes@users.noreply.github.com>
2024-11-12 14:38:05 +0100

    chore(module01): update ex00

--- module01/ex00/main.cpp
+++ module01/ex00/main.cpp
@@ -6,13 +6,13 @@
 /*   By: sabras <sabras@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 13:50:37 by sabras            #+#    #+#             */
-/*   Updated: 2024/11/06 16:02:29 by sabras           ###   ########.fr       */
+/*   Updated: 2024/11/12 14:37:41 by sabras           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-int	main(void) {
+int main(void) {
 	Zombie *zombie = newZombie("Simon");
 	zombie->announce();
 	delete zombie;
