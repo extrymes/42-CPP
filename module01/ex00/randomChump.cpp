57e61c46d1c398153a484170912b568cf058f9e9
extrymes <65893656+extrymes@users.noreply.github.com>
2024-11-13 20:54:58 +0100

    fix(module01): alloc zombie on the stack with `randomChump()` function

--- module01/ex00/randomChump.cpp
+++ module01/ex00/randomChump.cpp
@@ -6,15 +6,14 @@
 /*   By: sabras <sabras@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 15:43:27 by sabras            #+#    #+#             */
-/*   Updated: 2024/11/06 15:55:34 by sabras           ###   ########.fr       */
+/*   Updated: 2024/11/13 20:46:51 by sabras           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
 void randomChump(std::string name) {
-	Zombie *zombie = new Zombie();
-	zombie->setName(name);
-	zombie->announce();
-	delete zombie;
+	Zombie zombie;
+	zombie.setName(name);
+	zombie.announce();
 }
