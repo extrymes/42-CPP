bd544840668df6b044a7fcaeccc799bc8498fe36
extrymes <65893656+extrymes@users.noreply.github.com>
2024-11-15 05:25:35 +0100

    chore(module02): add ex02

---
+++ module02/ex02/src/Fixed.cpp
@@ -0,0 +1,151 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sabras <sabras@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/14 21:36:26 by sabras            #+#    #+#             */
+/*   Updated: 2024/11/15 05:23:37 by sabras           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/Fixed.hpp"
+
+Fixed::Fixed(void) {
+	_rawBits = 0;
+}
+
+Fixed::Fixed(const int value) {
+	_rawBits = value << _nbFracBits;
+}
+
+Fixed::Fixed(const float value) {
+	_rawBits = roundf(value * (1 << _nbFracBits));
+}
+
+Fixed::Fixed(const Fixed &other) {
+	*this = other;
+}
+
+Fixed::~Fixed(void) {}
+
+Fixed &Fixed::operator=(const Fixed &other) {
+	_rawBits = other.getRawBits();
+	return *this;
+}
+
+bool Fixed::operator>(const Fixed &other) {
+	return _rawBits > other.getRawBits();
+}
+
+bool Fixed::operator<(const Fixed &other) {
+	return _rawBits < other.getRawBits();
+}
+
+bool Fixed::operator>=(const Fixed &other) {
+	return _rawBits >= other.getRawBits();
+}
+
+bool Fixed::operator<=(const Fixed &other) {
+	return _rawBits <= other.getRawBits();
+}
+
+bool Fixed::operator==(const Fixed &other) {
+	return _rawBits == other.getRawBits();
+}
+
+bool Fixed::operator!=(const Fixed &other) {
+	return _rawBits != other.getRawBits();
+}
+
+Fixed Fixed::operator+(const Fixed &other) {
+	Fixed res;
+	res.setRawBits(_rawBits + other.getRawBits());
+	return res;
+}
+
+Fixed Fixed::operator-(const Fixed &other) {
+	Fixed res;
+	res.setRawBits(_rawBits - other.getRawBits());
+	return res;
+}
+
+Fixed Fixed::operator*(const Fixed &other) {
+	Fixed res;
+	res.setRawBits((_rawBits * other.getRawBits()) >> _nbFracBits);
+	return res;
+}
+
+Fixed Fixed::operator/(const Fixed &other) {
+	Fixed res;
+	res = ((_rawBits << _nbFracBits) / other.getRawBits());
+	return res;
+}
+
+Fixed &Fixed::operator++() {
+	_rawBits++;
+	return *this;
+}
+
+Fixed Fixed::operator++(int) {
+	Fixed tmp(*this);
+	_rawBits++;
+	return tmp;
+}
+
+Fixed &Fixed::operator--() {
+	_rawBits--;
+	return *this;
+}
+
+Fixed Fixed::operator--(int) {
+	Fixed tmp(*this);
+	_rawBits--;
+	return tmp;
+}
+
+Fixed &Fixed::min(Fixed &a, Fixed &b) {
+	if (a < b)
+		return a;
+	return b;
+}
+
+const Fixed &Fixed::min(const Fixed &a, const Fixed &b) {
+	if (a.getRawBits() < b.getRawBits())
+		return a;
+	return b;
+}
+
+Fixed &Fixed::max(Fixed &a, Fixed &b) {
+	if (a > b)
+		return a;
+	return b;
+}
+
+const Fixed &Fixed::max(const Fixed &a, const Fixed &b) {
+	if (a.getRawBits() > b.getRawBits())
+		return a;
+	return b;
+}
+
+int Fixed::getRawBits(void) const {
+	return _rawBits;
+}
+
+void Fixed::setRawBits(int const raw) {
+	_rawBits = raw;
+}
+
+float Fixed::toFloat(void) const {
+	return (float)(_rawBits / (float)(1 << _nbFracBits));
+}
+
+int Fixed::toInt(void) const {
+	return _rawBits >> _nbFracBits;
+}
+
+std::ostream& operator<<(std::ostream &out, const Fixed &fixed) {
+	out << fixed.toFloat();
+	return out;
+}
---
+++ module02/ex02/src/main.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sabras <sabras@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/14 22:08:17 by sabras            #+#    #+#             */
+/*   Updated: 2024/11/15 05:18:57 by sabras           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/Fixed.hpp"
+
+int main(void) {
+	Fixed a;
+	Fixed const b(Fixed(5.05f) * Fixed(2));
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+	std::cout << Fixed::max(a, b) << std::endl;
+	return 0;
+}
