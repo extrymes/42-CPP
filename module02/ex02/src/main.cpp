45c7b949816998e78df069879378fe876391e69a
extrymes <65893656+extrymes@users.noreply.github.com>
2024-11-21 16:02:08 +0100

    chore(module02): add more tests for ex02

--- module02/ex02/src/main.cpp
+++ module02/ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sabras <sabras@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 22:08:17 by sabras            #+#    #+#             */
-/*   Updated: 2024/11/19 15:30:17 by sabras           ###   ########.fr       */
+/*   Updated: 2024/11/21 16:01:08 by sabras           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,24 @@
 int main(void) {
 	Fixed a;
 	Fixed const b(Fixed(5.05f) * Fixed(2));
-	std::cout << a << std::endl;
-	std::cout << ++a << std::endl;
-	std::cout << a << std::endl;
-	std::cout << a++ << std::endl;
-	std::cout << a << std::endl;
-	std::cout << b << std::endl;
-	std::cout << Fixed::max(a, b) << std::endl;
-	std::cout << Fixed(10.5f) / Fixed(2) << std::endl;
+	Fixed c(10.5f);
+	Fixed d(2);
+	std::cout << "a: " << a << std::endl;
+	std::cout << "Pre-increment a: " << ++a << std::endl;
+	std::cout << "a: " << a << std::endl;
+	std::cout << "Post-increment a: " << a++ << std::endl;
+	std::cout << "a: " << a << std::endl;
+	std::cout << "Pre-decrement a: " << --a << std::endl;
+	std::cout << "a: " << a << std::endl;
+	std::cout << "Post-decrement a: " << a-- << std::endl;
+	std::cout << "a: " << a << std::endl;
+	std::cout << "b: " << b << std::endl;
+	std::cout << "c: " << c << std::endl;
+	std::cout << "c: " << d << std::endl;
+	std::cout << "max(b, c): " << Fixed::max(b, c) << std::endl;
+	std::cout << "c + d: " << c + d << std::endl;
+	std::cout << "c - d: " << c - d << std::endl;
+	std::cout << "c * d: " << c * d << std::endl;
+	std::cout << "c / d: " << c / d << std::endl;
 	return 0;
 }
