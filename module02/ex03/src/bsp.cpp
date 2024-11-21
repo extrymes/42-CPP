a4fc81f5262ff14ca9f74b34dfa01ef5cde11a0c
extrymes <65893656+extrymes@users.noreply.github.com>
2024-11-21 16:02:15 +0100

    chore(module02): update ex03

--- module02/ex03/src/bsp.cpp
+++ module02/ex03/src/bsp.cpp
@@ -6,13 +6,30 @@
 /*   By: sabras <sabras@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 05:11:58 by sabras            #+#    #+#             */
-/*   Updated: 2024/11/19 15:29:27 by sabras           ###   ########.fr       */
+/*   Updated: 2024/11/21 15:31:36 by sabras           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/Point.hpp"
 
-Fixed calculateArea(Point const a, Point const b, Point const c) {
+static Fixed calculateArea(Point const a, Point const b, Point const c);
+static Fixed absValue(Fixed value);
+
+bool bsp(Point const a, Point const b, Point const c, Point const point) {
+	Fixed totalArea = calculateArea(a, b, c);
+	Fixed area1 = calculateArea(a, b, point);
+	Fixed area2 = calculateArea(a, c, point);
+	Fixed area3 = calculateArea(b, c, point);
+	Fixed precision(0.00390625f);
+
+	if (area1 == 0 || area2 == 0 || area3 == 0)
+		return false;
+	if (absValue(totalArea - (area1 + area2 + area3)) > precision)
+		return false;
+	return true;
+}
+
+static Fixed calculateArea(Point const a, Point const b, Point const c) {
 	Fixed res = (a.getX() * (b.getY() - c.getY())
 		+ (b.getX() * (c.getY() - a.getY()))
 		+ (c.getX() * (a.getY() - b.getY())))
@@ -22,15 +39,8 @@ Fixed calculateArea(Point const a, Point const b, Point const c) {
 	return res;
 }
 
-bool bsp(Point const a, Point const b, Point const c, Point const point) {
-	Fixed totalArea = calculateArea(a, b, c);
-	Fixed area1 = calculateArea(a, b, point);
-	Fixed area2 = calculateArea(a, c, point);
-	Fixed area3 = calculateArea(b, c, point);
-
-	if (area1 == 0 || area2 == 0 || area3 == 0)
-		return false;
-	if (totalArea != (area1 + area2 + area3))
-		return false;
-	return true;
+static Fixed absValue(Fixed value) {
+	if (value < 0)
+		value = Fixed(0) - value;
+	return value;
 }
--- module02/ex03/src/main.cpp
+++ module02/ex03/src/main.cpp
@@ -6,23 +6,33 @@
 /*   By: sabras <sabras@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 23:54:03 by sabras            #+#    #+#             */
-/*   Updated: 2024/11/18 16:29:39 by sabras           ###   ########.fr       */
+/*   Updated: 2024/11/21 15:57:51 by sabras           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/Point.hpp"
 
-int main(void) {
+int main( void ) {
 	Point a(0, 4);
 	Point b(4, -4);
 	Point c(10.25, 3.56);
-	Point point(6, 2);
-	bool found;
+	float x[] = {2, 0};
+	float y[] = {2, 6, 10, 14, 0, 2, 4, 8};
+	int j = 0;
+	int i = 0;
 
-	found = bsp(a, b, c, point);
-	if (found)
-		std::cout << "Point found in triangle!" << std::endl;
-	else
-		std::cout << "Point not found in triangle!" << std::endl;
+	while (i < 8)
+	{
+		Point d(y[i], x[j]);
+		std::cout << "y = " << y[i] << std::endl;
+		std::cout << "x = " << x[j] << std::endl;
+		if (bsp(a, b, c, d))
+			std::cout << "YES, your point is within the triangle!" << std::endl;
+		else
+			std::cout << "NO, your point is not within the triangle!" << std::endl;
+		i++;
+		if (i == 4 && j == 0)
+			j = 1;
+	}
 	return 0;
 }
