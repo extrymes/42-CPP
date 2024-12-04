274a220f40badcf89e025d70d996e0e1d3e9a8c4
extrymes <65893656+extrymes@users.noreply.github.com>
2024-12-04 11:19:21 +0100

    chore(module05): update ex00

--- module05/ex00/src/main.cpp
+++ module05/ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sabras <sabras@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/01 19:52:48 by sabras            #+#    #+#             */
-/*   Updated: 2024/12/02 16:18:14 by sabras           ###   ########.fr       */
+/*   Updated: 2024/12/04 11:14:10 by sabras           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,35 +14,39 @@
 #include "../includes/colors.h"
 
 int main() {
-	Bureaucrat bob;
-
 	std::cout << YELLOW "--- Test 1 ---" RESET << std::endl;
-	Bureaucrat max("Max", 5);
 	try {
-		std::cout << YELLOW "--- Test 2 ---" RESET << std::endl;
-		Bureaucrat crow("Crow", 0); // would not work
+		Bureaucrat bob("Bob", 5);
 		std::cout << BLUE << bob << RESET << std::endl;
 	} catch (std::exception &e) {
 		std::cerr << RED "Error: " << e.what() << RESET << std::endl;
 	}
-
-	std::cout << BLUE << bob << RESET << std::endl;
+	std::cout << YELLOW "--- Test 2 ---" RESET << std::endl;
 	try {
-		std::cout << YELLOW "--- Test 3 ---" RESET << std::endl;
+		Bureaucrat crow("Crow", 0); // would not work
+		std::cout << BLUE << crow << RESET << std::endl;
+	} catch (std::exception &e) {
+		std::cerr << RED "Error: " << e.what() << RESET << std::endl;
+	}
+	std::cout << YELLOW "--- Test 3 ---" RESET << std::endl;
+	try {
+		Bureaucrat bob("Bob", 150);
 		bob.decrementGrade(); // would not work
 		std::cout << BLUE << bob << RESET << std::endl;
 	} catch (std::exception &e) {
 		std::cerr << RED "Error: " << e.what() << RESET << std::endl;
 	}
+	std::cout << YELLOW "--- Test 4 ---" RESET << std::endl;
 	try {
-		std::cout << YELLOW "--- Test 4 ---" RESET << std::endl;
+		Bureaucrat bob("Bob", 5);
 		bob.incrementGrade();
 		std::cout << BLUE << bob << RESET << std::endl;
 	} catch (std::exception &e) {
 		std::cerr << RED "Error: " << e.what() << RESET << std::endl;
 	}
+	std::cout << YELLOW "--- Test 5 ---" RESET << std::endl;
 	try {
-		std::cout << YELLOW "--- Test 5 ---" RESET << std::endl;
+		Bureaucrat max("Max", 5);
 		max.incrementGrade();
 		std::cout << BLUE << max << RESET << std::endl;
 		max.incrementGrade();
@@ -56,15 +60,17 @@ int main() {
 	} catch (std::exception &e) {
 		std::cerr << RED "Error: " << e.what() << RESET << std::endl;
 	}
+	std::cout << YELLOW "--- Test 6 ---" RESET << std::endl;
 	try {
-		std::cout << YELLOW "--- Test 6 ---" RESET << std::endl;
+		Bureaucrat bob("Bob", 5);
 		bob.incrementGrade();
 		std::cout << BLUE << bob << RESET << std::endl;
 	} catch (std::exception &e) {
 		std::cerr << RED "Error: " << e.what() << RESET << std::endl;
 	}
+	std::cout << YELLOW "--- Test 7 ---" RESET << std::endl;
 	try {
-		std::cout << YELLOW "--- Test 7 ---" RESET << std::endl;
+		Bureaucrat bob("Bob", 4);
 		bob.decrementGrade();
 		std::cout << BLUE << bob << RESET << std::endl;
 	} catch (std::exception &e) {
